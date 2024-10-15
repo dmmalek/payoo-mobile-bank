d359b295c65a84fc64df102215b6b6ac4da62408
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-10-15 22:23:23 +0600

    setup login page

--- login.js
+++ login.js
@@ -1,9 +1,19 @@
 document.getElementById('login-btn').addEventListener("click", (event) => {
     event.preventDefault();
+    let phoneNumber = document.getElementById('phone-number').value;
+    let pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('Wrong Input');
+    }
 
 })
 
-document.getElementById('phone-number').addEventListener("keyup", (e) => {
-    let phoneNumber = e.target.value;
-    console.log(phoneNumber);
-})
+// document.getElementById('phone-number').addEventListener("keyup", (e) => {
+//     let phoneNumber = e.target.value;
+//     console.log(phoneNumber);
+// })
